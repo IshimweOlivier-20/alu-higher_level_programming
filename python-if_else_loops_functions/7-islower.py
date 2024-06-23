98eeea48accc0e519ac3ab18c7c9df6340e485b4
IshimweOlivier-20 <o.ishimwe@alustudent.com>
2024-06-23 13:36:18 -0700

    Error

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
     return ord('a') <= ord(c) <= ord('z')
-print(islower('a'))  # True
-print(islower('A'))  # False
-print(islower('z'))  # True
-print(islower('Z'))  # False
-print(islower('5'))  # False
+print("a is {}".format("lower" if islower("a") else "upper"))
+print("H is {}".format("lower" if islower("H") else "upper"))
+print("A is {}".format("lower" if islower("A") else "upper"))
+print("3 is {}".format("lower" if islower("3") else "upper"))
+print("g is {}".format("lower" if islower("g") else "upper"))
