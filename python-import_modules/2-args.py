3fa2d7320f163ba9e13d8d50109f47365dabd987
IshimweOlivier-20 <o.ishimwe@alustudent.com>
2024-06-24 07:59:01 -0700

    Error

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     x = len(argv) - 1
     if x < 1:
-        print("{} argument.".format(x))
+        print("0 arguments.")
     elif x == 1:
         print("{} argument:".format(x))
     else:
