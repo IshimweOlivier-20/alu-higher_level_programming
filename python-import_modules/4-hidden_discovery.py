eac724823add647cfbb9baca2dff9912fab18ba2
IshimweOlivier-20 <o.ishimwe@alustudent.com>
2024-06-24 08:56:32 -0700

    Error

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+if __name__ == "_main__":
     from hidden_4 import *
-
-    all_names = dir()
-    filtered_names = sorted(name for name in all_names if not name.startswith("__"))
-
-    for name in filtered_names:
-        print()
+    allf = dir()
+    for i in range(0, len(allf)):
+        if allf[i][:2] != "__":
+            print("{:s}".format(allf[i]))
