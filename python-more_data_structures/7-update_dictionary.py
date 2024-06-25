b91e73e34393aee9ec77cb5327c82d078d4ab279
IshimweOlivier-20 <o.ishimwe@alustudent.com>
2024-06-25 14:41:42 -0700

    Error

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+def print_sorted_dictionary(a_dictionary):
+    if a_dictionary is None:
+        print("Error: Dictionary is None.")
+        return
+    
+    sorted_keys = sorted(a_dictionary.keys())  # Sort keys alphabetically
+    
+    for key in sorted_keys:
+        print(f"{key}: {a_dictionary[key]}")
