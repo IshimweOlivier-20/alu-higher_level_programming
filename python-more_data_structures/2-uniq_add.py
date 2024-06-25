e7dc6cf78bad367a1229ce99c3f1d9e4a348b356
IshimweOlivier-20 <o.ishimwe@alustudent.com>
2024-06-25 13:59:09 -0700

    Task 2

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,8 @@
+#!/sur/bin/python3
+def uniq_add(my_list=[]):
+    unique_numbers = set()
+    for num in my_list:
+        unique_numbers.add(num)
+        sum_unique = sum(unique_numbers)
+
+    return sum_unique
