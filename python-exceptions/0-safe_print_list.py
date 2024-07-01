fa409c2895c66ee731a25543c5e4da9c61a839bd
IshimweOlivier-20 <o.ishimwe@alustudent.com>
2024-07-01 01:15:09 -0700

    Error

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,11 +2,13 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     ret = 0
-    for i in range(0, x):
-        try:
+    
+    try:
+        for i in range(0, x):
             print("{:d}".format(my_list[i]), end="")
             ret += 1
-        except (ValueError, TypeError):
-            continue
+    except (ValueError, TypeError):
+        pass
+    
     print("")
     return ret
