4eaa6aca9654bc5a9320be66ff960f21a2b1f0e6
IshimweOlivier-20 <o.ishimwe@alustudent.com>
2024-07-01 01:33:58 -0700

    Error

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
-    ret = 0
-    
+
+def safe_print_integer(value):
+    num = 0
     try:
-        for i in range(0, x):
-            print("{:d}".format(my_list[i]), end="")
-            ret += 1
-    except (ValueError, TypeError):
-        pass
-    
-    print("")
-    return ret
+        for item in range(x):
+            print("{}".format(my_list[item]), end='')
+            num += 1
+    except IndexError:
+          pass
+      print()
+      return num
