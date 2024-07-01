30a8752400883ed7c323d37e5c95be5e46c49da0
IshimweOlivier-20 <o.ishimwe@alustudent.com>
2024-07-01 10:29:31 -0700

    Error

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,12 +1,38 @@
 #!/usr/bin/python3
 
 class Rectangle:
+    """
+    This class defines a rectangle with width and height attributes.
+
+    Attributes:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+    """
+
     def __init__(self, width=0, height=0):
+        """
+        Initializes a rectangle with optional width and height.
+
+        Args:
+            width (int, optional): The width of the rectangle (default is 0).
+            height (int, optional): The height of the rectangle (default is 0).
+
+        Raises:
+            TypeError: If width or height is not an integer.
+            ValueError: If width or height is less than 0.
+        """
         self.width = width
         self.height = height
     
     @property
     def width(self):
+        """
+        int: The width of the rectangle.
+
+        Raises:
+            TypeError: If width is not an integer.
+            ValueError: If width is less than 0.
+        """
         return self.__width
     
     @width.setter
@@ -20,6 +46,13 @@ class Rectangle:
     
     @property
     def height(self):
+        """
+        int: The height of the rectangle.
+
+        Raises:
+            TypeError: If height is not an integer.
+            ValueError: If height is less than 0.
+        """
         return self.__height
     
     @height.setter
