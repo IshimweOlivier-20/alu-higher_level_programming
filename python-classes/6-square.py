626cc32ec29220067e017c86dc1579480d8e761a
IshimweOlivier-20 <o.ishimwe@alustudent.com>
2024-07-01 06:23:55 -0700

    Error

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -106,8 +106,11 @@ class Square:
         """
         if self.__size == 0:
             print()
-        else:
-            for _ in range(self.__position[1]):
-                print()
-            for _ in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+            return
+
+        for _ in range(self.__position[1]):
+            print()
+
+        square_line = " " * self.__position[0] + "#" * self.__size
+        for _ in range(self.__size):
+            print(square_line)
