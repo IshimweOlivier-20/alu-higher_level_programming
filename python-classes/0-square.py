d52b77f5069267b80919bd090b11c6b4cf76c6b9
IshimweOlivier-20 <o.ishimwe@alustudent.com>
2024-07-01 04:08:21 -0700

    Error

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""
+This module defines the Square class.
+
+The Square class represents a square and currently does not have any attributes or methods.
+"""
 class Square:
+   """
+    This class represents a square.
+
+    It currently does not have any attributes or methods.
+    """
     pass
