a63d43b73add57186b04bd62f234cdbeb17cfb10
IshimweOlivier-20 <o.ishimwe@alustudent.com>
2024-07-01 01:42:38 -0700

    Error

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-def safe_print_integer(value):
+def safe_print_list(my_list=[], x=0):
     num = 0
     try:
-        for item in range(x):
-            print("{}".format(my_list[item]), end='')
+        for i in range(x):
+            print("{}".format(my_list[i]), end='')
             num += 1
     except IndexError:
         pass
