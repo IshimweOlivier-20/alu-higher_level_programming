93a4a08b74772d1d559aa9204c5a1bd682bc0e2f
IshimweOlivier-20 <o.ishimwe@alustudent.com>
2024-07-15 05:35:06 -0700

    Task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""writes a string to a file and returns the num of chars written:"""
+"""Write a function that reads a text file (UTF8) and prints it to stdout:"""
 
 
-def write_file(filename="", text=""):
-    """Write a string to a UTF8 text file
-    """
-    with open(filename, "w", encoding="utf-8") as file:
-        return file.write(text)
+def read_file(filename=""):
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
