37d310d05b493945c0ffbfb4a6958a5cae857081
IshimweOlivier-20 <o.ishimwe@alustudent.com>
2024-07-15 06:47:01 -0700

    Task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""defining write_file with two arguments"""
+"""writes a string to a file and returns the num of chars written:"""
 
 
 def write_file(filename="", text=""):
-    """reads filename with utf-8"""
-    with open(filename, "w", encoding="utf-8") as f:
+    """Write a string to a UTF8 text file
+    """
+    with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
