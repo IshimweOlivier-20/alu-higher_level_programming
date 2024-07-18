1748ec7e7e0aa0b5849d1afb5565935fa681b4cc
IshimweOlivier-20 <o.ishimwe@alustudent.com>
2024-07-18 10:25:49 -0700

    Error

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
 -- A script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+CREATE USER 'user_0d_1'@'localhost'
+CREATE USER 'user_0d_2'@'localhost'
 SHOW GRANT FOR 'user_0d_1'@'localhost';
 SHOW GRANT FOR 'user_0d_2'@'localhost';
