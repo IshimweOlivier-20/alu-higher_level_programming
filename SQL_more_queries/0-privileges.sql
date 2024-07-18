e0584979910ed95fbff750ba30b9afba77157968
IshimweOlivier-20 <o.ishimwe@alustudent.com>
2024-07-18 10:18:09 -0700

    Task 1

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
-A script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
-GRANT *
+-- A script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+SHOW GRANT FOR 'user_0d_1'@'localhost';
+SHOW GRANT FOR 'user_0d_2'@'localhost';
