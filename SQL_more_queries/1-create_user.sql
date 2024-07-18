5eb8ab113ded3ca947311622826ab6c30ee9bee0
IshimweOlivier-20 <o.ishimwe@alustudent.com>
2024-07-18 11:03:38 -0700

    Task1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- A script that creates the MySQL server user user_0d_1.
-CREATE USER IF NOT EXISTS 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd'
-SHOW GRANT FOR ''user_0d_1'
+CREATE USER IF NOT EXISTS 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd;
+GRANT ALL PREVILEGES ON *.* TO 'user_0d_1'@'localhost';
