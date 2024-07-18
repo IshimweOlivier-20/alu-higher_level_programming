7b3b4591d536bfdf69362c55c6f82a1acf6b0fce
IshimweOlivier-20 <o.ishimwe@alustudent.com>
2024-07-18 15:31:59 -0700

    Task9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Write a script that lists all cities contained in the database hbtn_0d_usa.
-SELECT c.id, c.name, s.name
-FROM FROM cities AS C
+SELECT C.id, C.name, S.name 
+FROM cities AS C
 JOIN states AS S ON S.id = C.state_id
 ORDER BY C.id;
