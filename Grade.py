cd3fd726526eb5d27d62a40ce635c1a85e87692d
IshimweOlivier-20 <o.ishimwe@alustudent.com>
2024-07-23 10:21:52 +0000

    Grade.py

--- Grade.py
+++ Grade.py
@@ -78,7 +78,7 @@ class GradeBook:
             print("............................................................")
             print("                Welcome to Our Student Transcript              ")
             print("............................................................")
-            print(f"Student name: {'.' * (25 - len('Student name: ') - len(student.name))} {student.name}")
+            print(f"Student name: {'.' * (25 - len('Student name: ' + student.names))} {student.names}")
 
 
 
