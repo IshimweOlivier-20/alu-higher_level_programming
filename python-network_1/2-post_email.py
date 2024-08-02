e0d6c355d6e7b02cc4bed87b24145938bc67c3de
IshimweOlivier-20 <o.ishimwe@alustudent.com>
2024-08-02 10:57:26 +0000

    2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """My Documentation """"
-import urllib.request
-import urllib.parse
 import sys
+import urllib.parse
+import urllib.request
 
-if __name__ == '__main__':
-    """"Documented"""
+
+if __name__ == "__main__":
     url = sys.argv[1]
-    values = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(values)
-    data = data.encode('ascii')  # data should be bytes
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as response:
-        content = response.read()
-        print("{}".format(content.decode("utf-8")))
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
