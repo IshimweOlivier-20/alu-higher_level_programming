4693be54b59fc726165b5c6297d1ef113eb60239
IshimweOlivier-20 <o.ishimwe@alustudent.com>
2024-08-02 10:13:38 +0000

    5-post_params.sh

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #comment
-curl -s -X POST "$URL" -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
