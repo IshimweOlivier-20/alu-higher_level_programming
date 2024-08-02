1378d46c12d90302f296200ead50d5dca4058ebb
IshimweOlivier-20 <o.ishimwe@alustudent.com>
2024-08-02 10:28:43 +0000

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,13 +4,11 @@ and displays the value of the 'X-Request-Id' header found in the response."""
 
 import urllib.request
 
-# Define the URL to fetch
-url = 'https://alu-intranet.hbtn.io/status'
 
-# Open the URL and read the response
-with urllib.request.urlopen(url) as response:
-    headers = response.info()
-
-# Extract and print the 'X-Request-Id' header
-request_id = headers.get('X-Request-Id')
-print(f'X-Request-Id: {request_id}')
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
