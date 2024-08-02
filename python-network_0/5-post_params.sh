952b6a30cb0ee15be536c32eae02200b525fc83f
IshimweOlivier-20 <o.ishimwe@alustudent.com>
2024-08-02 10:09:37 +0000

    5-post_params.sh

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!bin/bash
-#sends a POST request to the passed URL, and displays the body of the response.
+#comment
 curl -s -X POST "$URL" -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
