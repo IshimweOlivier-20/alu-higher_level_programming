cdff0b47d7f295c0e9ddc0324028a87d742b8706
IshimweOlivier-20 <o.ishimwe@alustudent.com>
2024-08-02 10:41:50 +0000

    1-hbtn_header.py

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+"""My documentation"""
+import sys
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
