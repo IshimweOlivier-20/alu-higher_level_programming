2e18517c89257ff4329a723beecb0be7c05527bf
IshimweOlivier-20 <o.ishimwe@alustudent.com>
2024-09-24 22:38:01 +0000

    2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 if (args.lenght === 0); {
-console.log('No argument');
+	console.log('No argument');
 } else if (args.lenght === 1); {
-console.log('Argument found');
+	console.log('Argument found');
 } else {
-console.log('Arguments found');
+	console.log('Arguments found');
 }  
