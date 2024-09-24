2ad40f468eeae18cf321bb68e4a19edcfbd5379c
IshimweOlivier-20 <o.ishimwe@alustudent.com>
2024-09-24 23:23:24 +0000

    Fixing Error

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
-t languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+#!/usr/bin/node
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < languages.length; i++) {
   console.log(languages[i]);
 }
