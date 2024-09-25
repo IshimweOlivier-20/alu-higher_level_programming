2e1a920390325660b973a3369dba6f162ae312f4
IshimweOlivier-20 <o.ishimwe@alustudent.com>
2024-09-25 01:57:10 +0000

    Error

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,16 +1,8 @@
 #!/usr/bin/node
-const prevSquare = require('./5-square');
-class Square extends prevSquare {
-  // Method to print Square instance using given character or 'X'
-  charPrint (c = 'X') {
-    let row = '';
-    let i = 0;
-    for (i = 0; i < this.width; i++) {
-      row += c;
-    }
-    for (i = 0; i < this.height; i++) {
-      console.log(row);
-    }
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
   }
 }
 module.exports = Square;
